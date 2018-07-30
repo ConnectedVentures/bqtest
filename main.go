dbbabfff355c9827362bf0c97e7241bb535e29e3
Nathan Fisher <nfisher@junctionbox.ca>
2018-07-30 16:41:27 -0300

    Use old v2 legacy API

--- main.go
+++ main.go
@@ -6,17 +6,99 @@ import (
 
 	"cloud.google.com/go/bigquery"
 	"fmt"
-	"time"
+		"golang.org/x/oauth2/google"
+	"golang.org/x/oauth2"
+	bq "google.golang.org/api/bigquery/v2"
+	"encoding/hex"
+	"crypto/rand"
+	"net/http"
 )
 
 var (
 	projectID   = "fresh-8-testing"
 	dataSetName = "lab_lee"
+	tableName = "array_test_inferred_1532701620" // fmt.Sprintf("array_test_inferred_%v", time.Now().Unix())
 )
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.LUTC | log.Lshortfile)
 
+	oldAndBusted()
+}
+
+func oldAndBusted() {
+	client, err := google.DefaultClient(oauth2.NoContext, bq.BigqueryInsertdataScope)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	service, err := bq.New(client)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	// passes as expected
+	oldPutItem(service, []Item{{AnArray: []NumRecord{{Number: 42}}}})
+	fmt.Println("42 passed")
+
+	// fails unexpectedly
+	oldPutItem(service, []Item{{AnArray: make(NumRecords,0)}})
+	fmt.Println("empty passed")
+
+	// huh does not fail...
+	oldPutItem(service, []Item{{}})
+	fmt.Println("nil passed")
+}
+
+func oldPutItem(service *bq.Service, items []Item) {
+	rows := make([]*bq.TableDataInsertAllRequestRows, 0, len(items))
+	for _, item := range items {
+
+
+		iid, err := uuid()
+		if err != nil {
+			log.Fatalf("%v", err)
+		}
+
+		rows = append(rows, &bq.TableDataInsertAllRequestRows{
+			Json: map[string]bq.JsonValue{
+				"doc":      item,
+				"insertID": iid,
+			},
+		})
+	}
+
+	req := &bq.TableDataInsertAllRequest{
+		Rows: rows,
+	}
+
+	tableDataService := bq.NewTabledataService(service)
+
+	call := tableDataService.InsertAll(projectID, dataSetName, tableName, req)
+	resp, err := call.Do()
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	if resp.HTTPStatusCode != http.StatusOK {
+		log.Println(resp.HTTPStatusCode)
+	}
+}
+
+func uuid() (string, error) {
+	var u [16]byte
+	_, err := rand.Read(u[:])
+	if err != nil {
+		return "", err
+	}
+
+	u[8] = (u[8] | 0x80) & 0xBF
+	u[6] = (u[6] | 0x40) & 0x4F
+
+	return hex.EncodeToString(u[:]), nil
+}
+
+func newHotness() {
 	ctx := context.Background()
 	client, err := bigquery.NewClient(ctx, projectID)
 	if err != nil {
@@ -25,7 +107,7 @@ func main() {
 
 	myDataset := client.Dataset(dataSetName)
 
-	table := myDataset.Table(fmt.Sprintf("array_test_inferred_%v", time.Now().Unix()))
+	table := myDataset.Table(tableName)
 
 	schema, err := bigquery.InferSchema(Item{})
 	if err != nil {
